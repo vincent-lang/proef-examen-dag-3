8d3294405a606b135673632a33287ff5c3556b97
vincent van de merwe <335479@student.mboutrecht.nl>
2024-04-08 10:47:42 +0200

    added tables that i am going to use and with all there columns

---
+++ database/migrations/2024_04_08_073918_create_reserverings_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reserverings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('persoon_id');
+            $table->integer('openingstijd_id');
+            $table->integer('tarief_id');
+            $table->integer('baan_id');
+            $table->integer('pakketOptie_id')->nullable();
+            $table->integer('reserveringStatus_id');
+            $table->string('Reserveringsnummer');
+            $table->date('Datum');
+            $table->string('AantalUren');
+            $table->time('BeginTijd');
+            $table->time('EindTijd');
+            $table->integer('AantalVolwassenen');
+            $table->string('AantalKinderen')->nullable();
+            $table->boolean('IsActief');
+            $table->string('Opmerkingen')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reserverings');
+    }
+};
---
+++ database/migrations/2024_04_08_073934_create_persoons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('persoons', function (Blueprint $table) {
+            $table->id();
+            $table->integer('typePersoon_id');
+            $table->string('Voornaam');
+            $table->string('Tussenvoegsel')->nullable();
+            $table->string('Achternaam');
+            $table->string('Roepnaam');
+            $table->boolean('IsVolwassen');
+            $table->boolean('IsActief');
+            $table->string('Opmerkingen')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('persoons');
+    }
+};
